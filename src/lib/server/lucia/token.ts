4d4048816353a13a01f043357929420e75394c1b
rice-cracker-dev <ricecracker2234@outlook.com>
2023-10-28 16:08:36 +0700

    fixed db

--- src/lib/server/lucia/token.ts
+++ src/lib/server/lucia/token.ts
@@ -1,4 +1,4 @@
-import { generateRandomString, isWithinExpiration } from "lucia/utils";
+import { generateRandomString, isWithinExpiration } from 'lucia/utils';
 import { prisma } from '$lib/server/prisma';
 
 const EXPIRES_IN = 1000 * 60 * 60 * 2; // 2 hours
@@ -6,8 +6,8 @@ const EXPIRES_IN = 1000 * 60 * 60 * 2; // 2 hours
 // TODO: add email verification
 export const generateEmailVerificationToken = async (userId: string) => {
   const storedUserTokens = await prisma.emailVerificationToken.findMany({
-    where: { user_id: userId }
-  })
+    where: { user_id: userId },
+  });
   if (storedUserTokens.length > 0) {
     const reusableStoredToken = storedUserTokens.find((token) => {
       // check if expiration is within 1 hour
@@ -21,23 +21,23 @@ export const generateEmailVerificationToken = async (userId: string) => {
     data: {
       id: token,
       expires: new Date().getTime() + EXPIRES_IN,
-      user_id: userId
-    }
+      user_id: userId,
+    },
   });
 
   return token;
-}
+};
 
 export const validateEmailVerificationToken = async (token: string) => {
   const storedToken = await prisma.$transaction(async (trx) => {
     const storedToken = await trx.emailVerificationToken.findUnique({ where: { id: token } });
-    if (!storedToken) throw new Error("Invalid token");
+    if (!storedToken) throw new Error('Invalid token');
     await trx.emailVerificationToken.deleteMany({ where: { user_id: storedToken.user_id } });
     return storedToken;
   });
   const tokenExpires = Number(storedToken.expires); // bigint => number conversion
   if (!isWithinExpiration(tokenExpires)) {
-    throw new Error("Expired token");
+    throw new Error('Expired token');
   }
   return storedToken.user_id;
 };
